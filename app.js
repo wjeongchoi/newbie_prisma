7a2a6dfa32ac306fa4067f21fce79b2bc76408dc
Woojeong Choi <gldeel@kaist.ac.kr>
2024-05-01 22:47:46 +0900

    prob1

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const express = require('express');
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
 const app = express();
+const prisma = new PrismaClient();
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello');
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/problems/1", async (req, res) => {
+  const result = await prisma.customer.findMany({
+    where: {
+      income: {
+        gte: 50000,
+        lte: 60000,
+      },
+    },
+    orderBy: [
+      {
+        income: "desc",
+      },
+      {
+        lastName: "asc",
+      },
+      {
+        firstName: "asc",
+      },
+    ],
+    take: 10, 
+    select: {
+      firstName: true,
+      lastName: true,
+      income: true,
+    },
+  });
+  res.json(result);
 });
 
 app.listen(port, () => {
-    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+  console.log(`서버가 실행됩니다. http://localhost:${port}`);
 });
